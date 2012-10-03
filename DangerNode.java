1f9e7eec31151308689e563dfe432b08f1ef71ad
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-02 23:24:47 -0700

    added testing code and fixed simple search, added a getter. Going to add documentation and another constructor

--- DangerNode.java
+++ DangerNode.java
@@ -9,7 +9,7 @@ class DangerNode{
 	private Timestamp timestamp;
 	private DangerNode left = null;
 	private DangerNode right = null;
-	private float[] coordinates;
+	private float[] coordinates = new float[2];
 
 	/**
 	*Creates an instance of the DangerNode. Nulls may be passed for lChild and rChild if there are no children to this node
@@ -47,7 +47,7 @@ class DangerNode{
 
 	/**
 	*Searches the tree for the searchTuple given, returns null if not found and a DangerNode if found. Note this search is note the nearest neighbor search
-	*@param searchTuple The tuple of floats we're searching from.
+	*@param searchTuple The tuple of floats we're searching for.
 	*@return null if not found, the DangerNode if found.
 	*/
 	public DangerNode search(float[] searchTuple){
@@ -57,28 +57,87 @@ class DangerNode{
 
 	/**
 	*Search used by search function to search along the correct axis given the depth of the tree
+	*@param searchTuple the tuple of floats we're searching for.
+	*@param depth The depth of the tree, required to compute the proper sorting axis of the tree
+	*@return null if not found, the DangerNode if found.
 	*/
 	private DangerNode innerSearch(float[] searchTuple, int depth){
+		//Check for match
+		if(coordinates[(depth % coordinates.length)] == searchTuple[depth % searchTuple.length]){
+			System.out.println("partial match on depth" + depth);
+			if(coordinates[(depth +1)% coordinates.length] == searchTuple[(depth +1)% searchTuple.length]){
+					System.out.println("Total match");
+					return this;
+			}
+		}
 		//To the left, to the left...
 		if(coordinates[depth % coordinates.length] < searchTuple[depth % searchTuple.length]){
+			System.out.println("Traversing left");
 			if(this.left != null){
-				return this.left.innerSearch(searchTuple,depth-1);
-			}else{
-				return null;
-			}
-		}else if(coordinates[depth % coordinates.length] > searchTuple[depth % searchTuple.length]){
-			if(this.right != null){
-				return this.right.innerSearch(searchTuple,depth-1);
+				return this.left.innerSearch(searchTuple,depth+1);
 			}
 		}else{
-			//Then we must be equal along the axis we're searching for and should check the other 
-			//axis to see if we have an exact match.
-			if(coordinates[depth +1% coordinates.length] == searchTuple[depth +1% searchTuple.length]){
-				return this;
+			System.out.println("Traversing right");
+			if(this.right != null){
+				return this.right.innerSearch(searchTuple,depth+1);
 			}
 		}
 		return null;
 	}
 
-	
+	/**
+	*Gets the coordinate at the specified index. 
+	*@param index Since coordinates are a 2-Tuple, should be either 0 or 1
+	*@return The coordinate at that index.
+	*/
+	public float getCoordinate(int index){
+		return coordinates[index];
+	}
+
+	public void addNode(DangerNode newNode){
+		this.innerAddNode(newNode, 1);
+	}
+
+	public void innerAddNode(DangerNode newNode, int depth){
+		if(coordinates[depth % coordinates.length] < newNode.getCoordinate(depth % coordinates.length)){
+			if(this.left != null){
+				this.left.innerAddNode(newNode,depth+1);
+			}else{
+				this.left = newNode;
+			}
+		}else{
+			if(this.right != null){
+				this.right.innerAddNode(newNode,depth+1);
+			}else{
+				this.right = newNode;
+			}
+		}
+	}
+
+
+	//Test function
+	public static void main(String argv[]) throws Exception
+	{
+		DangerNode p = new DangerNode(1,1,1,null,null);
+		p.addNode(new DangerNode(0,1,1,null,null));
+		p.addNode(new DangerNode(3,3,3,null,null));
+		float [] s = new float[2];
+		s[0] = 1;
+		s[1] = 1;
+		System.out.println("Searching for 1,1");
+		System.out.println(p.search(s));
+		s[0] = 0;
+		s[1] = 1;
+		System.out.println("Searching for 0,1");
+		System.out.println(p.search(s));
+		s[0] = 3;
+		s[1] = 3;
+		System.out.println("Searching for 3,3");
+		System.out.println(p.search(s));
+		s[0] = 34;
+		s[1] = -34;
+		System.out.println("Searching for bogus value");
+		System.out.println(p.search(s));
+	}	
+
 }
