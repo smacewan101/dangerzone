2f3d213671800655213d5110ce3a027c1e0643a7
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-02 22:59:39 -0700

    added gitignore for class files and also put in basic search for tree, need to test first

--- DangerNode.java
+++ DangerNode.java
@@ -44,5 +44,41 @@ class DangerNode{
 		this(longitude,latitude,id,lChild,rChild);
 		this.timestamp = tStamp;
 	}
+
+	/**
+	*Searches the tree for the searchTuple given, returns null if not found and a DangerNode if found. Note this search is note the nearest neighbor search
+	*@param searchTuple The tuple of floats we're searching from.
+	*@return null if not found, the DangerNode if found.
+	*/
+	public DangerNode search(float[] searchTuple){
+		//Possibly thinking about having this return the id for the danger zone instead, will have to talk with group about this
+		return this.innerSearch(searchTuple,1);
+	}
+
+	/**
+	*Search used by search function to search along the correct axis given the depth of the tree
+	*/
+	private DangerNode innerSearch(float[] searchTuple, int depth){
+		//To the left, to the left...
+		if(coordinates[depth % coordinates.length] < searchTuple[depth % searchTuple.length]){
+			if(this.left != null){
+				return this.left.innerSearch(searchTuple,depth-1);
+			}else{
+				return null;
+			}
+		}else if(coordinates[depth % coordinates.length] > searchTuple[depth % searchTuple.length]){
+			if(this.right != null){
+				return this.right.innerSearch(searchTuple,depth-1);
+			}
+		}else{
+			//Then we must be equal along the axis we're searching for and should check the other 
+			//axis to see if we have an exact match.
+			if(coordinates[depth +1% coordinates.length] == searchTuple[depth +1% searchTuple.length]){
+				return this;
+			}
+		}
+		return null;
+	}
+
 	
 }
