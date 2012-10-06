38f24c19d169f112d64f47f3c5c69b29ab55e6a3
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-05 19:47:39 -0700

    began playing with ports, I  can revieve a message but cannot send back something to php

--- danger_zone/DangerControl.java
+++ danger_zone/DangerControl.java
@@ -17,18 +17,61 @@ public class DangerControl{
 	public static void main(String argv[]) throws Exception
 	{
 		//5480 For Listening, 5481 to send back out
-		//ServerSocket listen = new ServerSocket(5480);
+		ServerSocket listen = new ServerSocket(5480);
+
 		//Time out for the server while I test so I don't have to kill it
 		//10 Seconds should be ok for now
-		long timeout = System.currentTimeMillis() + 1000*10;
-		//Socket to send back the data
-		//Socket response = new Socket("127.0.0.1",5481);
+		long timeout = System.currentTimeMillis() + 1000*5;
 
+		//Socket to send back the data php must be listening to this or we will get an exception
+		Socket response = null;
+		
+		Socket incoming = null;
 		while(System.currentTimeMillis() < timeout){
+			//Attempt to listen for a client
+			try{
+				incoming = listen.accept();
+				System.out.println("Recieved");
+			}catch(IOException e){
+				System.out.println("Error");
+				continue;
+			}
+
+			//Read incoming messages with autoflushing printwriter
+			PrintWriter out = null;
+			BufferedReader info = new BufferedReader(new InputStreamReader(incoming.getInputStream()));
+			String msg;
+
+			while((msg = info.readLine()) != null){
+				//Say it recieved it
+				try{
+					//Set to 5480 for testing so we'll just send back to ourselves
+					System.out.println(msg);
+					response = new Socket("127.0.0.1",5481);
+					out = new PrintWriter(response.getOutputStream(), true);
+					out.println("Hey you I'm a server rawr");
+					out.close();
+				}catch(java.net.ConnectException e){
+					System.err.println("Failed to open connection to php to send back data");
+					System.err.println(e.getMessage());
+					out.close();
+					info.close();
+					incoming.close();
+					System.exit(1);
+				}
+			}
+
+			incoming.close();
+			response.close();
+
 
 		}
-
+		listen.close();
 		System.out.print("done");
+
+
+
+		
 
 
 	}
---
+++ danger_zone/test.php
@@ -0,0 +1,28 @@
+<?php
+//Danger Socket Test
+
+//Send Text of "LON 91.12 LAT 40.78"
+$host = "127.0.0.1";  
+$hostport = 5480;  
+$resultport = 5481;
+$timeout = 30;  //timeout in seconds  
+  
+$socket =  fsockopen($host,$hostport,$errNum);
+
+if($socket){
+	fwrite($socket,"LON 91.12 LAT 40.78");
+	sleep(1);
+	$readsocket = fsockopen($host,$resultport,$errNum2);
+	if($readsocket){
+		echo $fread($readsocket,8192);
+		fclose($readsocket);
+	}else{
+		echo "Failed read with " . $errNum2 .'\n';
+	}
+
+}
+
+fclose($socket);
+echo "Completed with: " . $errNum;
+
+?>
