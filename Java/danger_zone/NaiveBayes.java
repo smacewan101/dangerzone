a0d0615e15839cde5de17b2370be438ddcb48cf2
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-17 16:57:49 -0700

    initial commit to bayes

---
+++ Java/danger_zone/NaiveBayes.java
@@ -0,0 +1,17 @@
+package danger_zone;
+import java.util.HashMap;
+import java.io.*;
+
+/**
+*@author Ethan Eldridge <ejayeldridge @ gmail.com>
+*@version 0.0
+*@since 2012-10-17
+*
+* Naive Bayes classifier to classify a tweet as a danger or not. 
+*/
+public class NaiveBayes{
+	/**
+	*
+	*/
+
+}
