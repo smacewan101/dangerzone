770a835c0ce9fba701b9fc5410e0577ad830bc7d
Scott MacEwan <wsmacewan101@gmail.com>
2012-10-24 15:28:34 -0400

    commit to delete original of files that were moved

--- twitterDangerUpdate.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('twitterDB.php');
-$db = new TwitterDB();
-foreach($_POST as $danger){
-	$tweet_data = explode(',', $danger);
-	$db->updateTweetDanger($tweet_data[1], $tweet_data[0]);
-}
-
-?>
