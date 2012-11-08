b56a1bc2dffe68a6f3e9a04db959ebc95306e955
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-11-08 00:02:08 -0800

    the udp works from test.php on the server itself, but not on my machine, could be an underlying os problem

--- Java/danger_zone/test.php
+++ Java/danger_zone/test.php
@@ -1,6 +1,29 @@
 <?php
 //Danger Socket Test
 
+$message = "LON 91.12 LAT 40.78 NUM 3\r";
+$domain = (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN' ? AF_INET : AF_INET);
+$socketHandle = socket_create($domain, SOCK_DGRAM, SOL_UDP);
+
+$serverIP = "127.0.0.1";
+$serverSendPort = 5480;
+$serverRecvPort = 5480;
+
+socket_bind($socketHandle, $serverIP, $serverSendPort);
+
+socket_sendto($socketHandle, $message, strlen($message), MSG_EOF, $serverIP, $serverSendPort);
+$message = 'KILLSERVER0x0000';
+socket_sendto($socketHandle, $message, strlen($message), MSG_EOF, $serverIP,$serverSendPort);
+$response = "";
+$date = date_create();
+while(date_timestamp_get($date)+1 > date_timestamp_get(date_create())){
+socket_recvfrom($socketHandle, $response, 100, MSG_EOF, $serverIP);
+echo $response;
+}
+var_dump($message);
+
+
+/*
 //Send Text of "LON 91.12 LAT 40.78"
 $host ="dangerzone.cems.uvm.edu";  
 $hostport = 5480;  
@@ -23,6 +46,5 @@ if(!$sock){
 	fwrite($sock, "KILLSERVER0x0000");
 	fclose($sock);
 }
-
-
+*/
 ?>
