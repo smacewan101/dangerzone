a9d9bba7323abb0bb824dd29a183903de8a1fb8d
Scott MacEwan <wsmacewan101@gmail.com>
2012-11-14 16:50:08 -0500

    Linked the twitter seach page to the update danger form

--- Twitter/twitterDangerUpdate.php
+++ Twitter/twitterDangerUpdate.php
@@ -2,8 +2,8 @@
 require_once('twitterDB.php');
 $db = new TwitterDB();
 foreach($_POST as $danger){
-	$tweet_data = explode(',', $danger);
+	$tweet_data = explode('!###!', $danger);
 	$db->updateTweetDanger($tweet_data[1], $tweet_data[0]);
 }
-
+echo 'Thank you for grabbing some training info, it is much appreciated.'
 ?>
--- Twitter/twitterDangerUpdateForm.php
+++ Twitter/twitterDangerUpdateForm.php
@@ -1,20 +1,52 @@
 <?php
-require_once('twitterDB.php');
+require_once("twitterQuery.php");
+require_once("twitterDB.php");
+$twitterQuery = new TwitterQuery();
 $db = new TwitterDB();
-$tweets = $db->selectNewTweets();
 
-echo '<form action="twitterDangerUpdate.php" method="post">';
-$i=0;
-foreach($tweets as $tweet){
-	//var_dump($tweet);
-	echo '<p>';
-	echo $i.' '.$tweet['text'];
-	echo '<br />';	
-	echo '<input type="radio" name="danger'.$i.'" value="0,'.$tweet['id_str'].'" checked="true">Not Dangerous</option>';
-	echo '<input type="radio" name="danger'.$i.'" value="1,'.$tweet['id_str'].'">Dangerous</opiton>';	
-	echo '</p>';
-	$i = $i +1;
+if(!array_key_exists('username', $_POST)){
+	$keywords = explode(' ', $_POST['keywords']);
+	$lat = $_POST['lat'];
+	$long = $_POST['long'];
+	$radius=$_POST['radius'];
+	$units =$_POST['units'];
+	$rpp =$_POST['rpp'];
+	if($lat != '' && $radius != ''){
+		$geo=array($lat,$long,implode('',array($radius,$units)));
+		$twitterQuery->setGeo($geo);
+	}
+	$twitterQuery->setKeywords($keywords);
+	$twitterQuery->setRpp($rpp);
+	$twitterQuery->constructSearch();
+	$tweets = $twitterQuery->searchResults();
+}else{
+	if($_POST['type'] == 'name'){
+		$tweets = $twitterQuery->getUserTimelineByName($_POST['username']);
+	}else{
+		$tweets = $twitterQuery->getUserTimelineByID($_POST['username']);
+	}
 }
-echo '<input type="submit"/>';
-echo '</form>';
+
+if(is_int($tweets)){
+	echo $tweets;
+}else{
+	echo '<form action="twitterDangerUpdate.php" method="post">';
+	$i=0;
+	foreach($tweets as $tweet){
+		//var_dump($tweet);
+		$text = $tweet->text;
+		$id_str = $tweet->id_str;		
+		echo '<p>';
+		echo $i;
+		echo '<br />';		
+		echo $text;
+		echo '<input type="radio" name="danger'.$i.'" value="0!###!'.$id_str.'" checked="true">Not Dangerous</option>';
+		echo '<input type="radio" name="danger'.$i.'" value="1!###!'.$id_str.'">Dangerous</opiton>';	
+		echo '</p>';
+		$i = $i +1;
+	}
+	$db->insertTweets($tweets,$twitterQuery->search);
+	echo '<input type="submit"/>';
+	echo '</form>';
+}
 ?>
